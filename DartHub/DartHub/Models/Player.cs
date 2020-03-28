208b2db5589fa37e603ba7fdf6bc70faa1bb9f2b
Wyatt Sawyer <wj18sawy@alum.siena.edu>
2020-03-28 17:26:27 -0400

    Renamed old directories to DartHub, changed counter page to be 301

---
+++ DartHub/DartHub/Models/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DartHub.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public string AvatarPath { get; set; }
+        public readonly string smallAvatarsPath = "/images/avatar/small/";
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+            AvatarPath = String.Concat(smallAvatarsPath, name, ".jpg");
+        }
+    }
+}
